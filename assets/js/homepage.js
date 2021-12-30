f118fcedb94decb216b99811a606ae96e7aed598
Hibo Ali <hiba.mumin22@gmail.com>
2021-12-30 15:22:22 -0600

    created a function to display repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,34 @@
+// Create two new variables to store a ref to the <form> el id of user-name
+// and to the input el with an id of username
+
+var userFormEl = document.querySelector("#user-form"); 
+
+var nameInputEl = document.querySelector("#username");
+
+
+// create function formSubmitHandler() to be executed upon a form submission 
+// browser event
+
+var formSubmittedHandler = function(event) {
+    // prevent page from refreshing 
+    event.preventDefault();
+    console.log(event);
+
+
+    // get value from input element
+    var username = nameInputEl.ariaValueMax.trim();
+
+    if(username) {
+        getUserRepos(username);
+
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+
+
+};
+
 
 // http request
 var getUserRepos = function(user) {
@@ -6,29 +37,30 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url 
-    fetch(apiUrl).then(function(response) {
+    fetch(apiUrl)
+    .then(function(response) {
+        //request was successful
         response.json().then(function(data) {
-            console.log(data);
+            
         });
     });
 
 };
-// getUserRepos("user");
+getUserRepos("data"); 
 
-// Create two new variables to store a ref to the <form> el id of user-name
-// and to the input el with an id of username
 
-var userFormEl = document.querySelector("#user-form"); 
 
-var nameInputEl = document.querySelector("#username");
+//Create function to display repos
 
-// create function formSubmitHandler() to be executed upon a form submission 
-// browser event
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
 
-var formSubmittedHandler = function(event) {
-    event.preventDefault();
-    console.log(event);
 };
+
+
+
+
 
 // add the submit event listener to the userFormEl
 userFormEl.addEventListener("submit", formSubmitHandler);
