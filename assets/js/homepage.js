625fdd1ec56dcf525fe2df2e37833b8ea6d04b0d
Hibo Ali <hiba.mumin22@gmail.com>
2021-12-30 12:09:58 -0600

    modified the html starter code, and created new variables and eventListener in homepage.js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -13,4 +13,22 @@ var getUserRepos = function(user) {
     });
 
 };
-getUserRepos("hibo-ali");
+// getUserRepos("user");
+
+// Create two new variables to store a ref to the <form> el id of user-name
+// and to the input el with an id of username
+
+var userFormEl = document.querySelector("#user-form"); 
+
+var nameInputEl = document.querySelector("#username");
+
+// create function formSubmitHandler() to be executed upon a form submission 
+// browser event
+
+var formSubmittedHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+};
+
+// add the submit event listener to the userFormEl
+userFormEl.addEventListener("submit", formSubmitHandler);
